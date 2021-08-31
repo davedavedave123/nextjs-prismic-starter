4b6187a8f4d9fa52003b387a631f5f2eec25233b
Dave <davebaxtermusic@gmail.com>
2021-08-31 23:29:57 +1200

    .

--- components/blog/BlogLink.js
+++ components/blog/BlogLink.js
@@ -6,6 +6,7 @@ import ImageCover from '../ImageCover';
 import trimText from '../../utils/trimText';
 import Link from 'next/link';
 import { linkResolver } from '../../prismic-configuration';
+import useBreakPoints from '../../hooks/useBreakPoints';
 
 const FeaturedImage = ({ image }) => {
   const imgRef = useRef(null);
@@ -28,33 +29,63 @@ const FeaturedImage = ({ image }) => {
   );
 };
 
-const Text = ({ title, content }) => (
-  <div className='w-full h-full p-20 relative flex justify-center items-center'>
-    <div className='relative text-center'>
-      <h2 className='p-0 m-0'>{RichText.asText(title)}</h2>
-      <div className='w-20 h-1 bg-yellow-300 mx-auto my-5' />
-      <p>{trimText(content[0].text, 200)}</p>
+const Text = ({ title, content }) => {
+  return (
+    <div
+      className={`relative w-full h-full p-20 flex justify-center items-center`}
+    >
+      <div className='relative text-center'>
+        <h2 className='p-0 m-0'>{RichText.asText(title)}</h2>
+        <div className='w-20 h-1 bg-yellow-300 mx-auto my-5' />
+        <p>{trimText(content[0].text, 200)}</p>
+      </div>
+    </div>
+  );
+};
+
+const Screen = () => (
+  <div className='absolute w-full h-full top-0 left-0 bg-black opacity-40' />
+);
+
+const Text_mobile = ({ title, content }) => (
+  <div className={`absolute w-full h-full top-0 left-0`}>
+    <div className='relative w-full h-full p-10 flex justify-center items-center'>
+      <Screen />
+      <div className='relative text-center text-white'>
+        <h2 className='p-0 m-0'>{RichText.asText(title)}</h2>
+        <div className='w-20 h-1 bg-yellow-300 mx-auto my-5' />
+        <p>{trimText(content[0].text, 200)}</p>
+      </div>
     </div>
   </div>
 );
 
 export default function BlogLink({ blog, index }) {
   const { data } = blog;
+  const { isLgDown } = useBreakPoints();
+
   return (
     <Link href={linkResolver(blog)}>
-      <a className='w-full relative grid grid-cols-2'>
-        {/* for alternating image/text sides */}
-        {index % 2 == 0 ? (
-          <>
+      <a className='w-full relative lg:grid grid-cols-2 grid-rows-1'>
+        {/* For mobile */}
+        {isLgDown && (
+          <div className='relative w-full'>
             <FeaturedImage image={data.featured_image} />
-            <Text title={data.title} content={data.content} />
-          </>
-        ) : (
-          <>
-            <Text title={data.title} content={data.content} />
-            <FeaturedImage image={data.featured_image} />
-          </>
+            <Text_mobile title={data.title} content={data.content} />
+          </div>
         )}
+        {/* For alternating image/text sides */}
+        {index % 2 == 0 && !isLgDown ? (
+          <>
+            <FeaturedImage image={data.featured_image} />
+            <Text title={data.title} content={data.content} />
+          </>
+        ) : index % 2 != 0 && !isLgDown ? (
+          <>
+            <Text title={data.title} content={data.content} />
+            <FeaturedImage image={data.featured_image} />
+          </>
+        ) : null}
       </a>
     </Link>
   );
--- pages/blog/[uid].js
+++ pages/blog/[uid].js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useGetStaticProps, useGetStaticPaths } from 'next-slicezone/hooks';
 import { RichText } from 'prismic-reactjs';
+import Prismic from '@prismicio/client';
 
 import { Client } from '../../utils/prismicHelpers';
 import Gallery from '../../components/blog/Gallery';
@@ -12,8 +13,15 @@ export default function BlogPage(props) {
   //   data: { gallery, featured_image, title, content },
   // } = props;
 
+  // const getTags = async () => {
+  //   console.log('gettings tags');
+  //   const tags = await Client().getTags('tractors');
+  //   console.log('tags:', tags);
+  // };
+
   useEffect(() => {
-    console.log('blog data', data);
+    console.log('blog props', props);
+    // getTags();
   }, []);
 
   return (
@@ -40,16 +48,43 @@ export default function BlogPage(props) {
   );
 }
 
-export const getStaticProps = useGetStaticProps({
-  client: Client(),
-  queryType: 'repeat',
-  type: 'blog',
-  apiParams({ params }) {
-    return {
-      uid: params.uid,
-    };
-  },
-});
+// export const getStaticProps = useGetStaticProps({
+//   client: Client(),
+//   queryType: 'repeat',
+//   type: 'blog',
+//   apiParams({ params }) {
+//     console.log('params uid:', params.uid);
+//     return {
+//       uid: params.uid,
+//     };
+//   },
+// });
+
+export async function getStaticProps({
+  params,
+  preview = null,
+  previewData = {},
+}) {
+  const { ref } = previewData;
+
+  const client = Client();
+
+  console.log('params uid:', params.uid);
+
+  const doc = await client.getByUID('blog', 'tractors');
+
+  const tags = await client.query(
+    Prismic.Predicates.at('document.tags', ['tractors'])
+  );
+
+  return {
+    props: {
+      ...doc,
+      taggedArticles: tags?.results ? tags.results : [],
+      preview,
+    },
+  };
+}
 
 export const getStaticPaths = useGetStaticPaths({
   client: Client(),
