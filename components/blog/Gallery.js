714d993372480c0fa3c082bc7329cadd2b716343
Dave <davebaxtermusic@gmail.com>
2021-10-01 14:05:17 +1300

    clean up

--- components/blog/Gallery.js
+++ components/blog/Gallery.js
@@ -4,10 +4,13 @@ import GalleryImage from './GalleryImage';
 export default function Gallery({ gallery }) {
   if (gallery[0].image?.url)
     return (
-      <div className='w-full h-1/4 lg:grid grid-cols-2 grid-rows-2'>
+      <div
+        className='w-full h-1/4 lg:grid grid-cols-2 grid-rows-2'
+        // style={{ maxWidth: 1500, margin: '0 auto' }}
+      >
+        {/* <div className='flex-1 bg-yellow-300 p-10 box-content'></div>
+        <div className='flex-1 bg-blue-300 p-10 box-content'></div> */}
         {gallery.map((galleryImage, index) => (
-          // <div>gallery</div>
-
           <GalleryImage
             key={`galleryImage-${index}`}
             galleryImage={galleryImage}
