e4be0917531a8575867c8e655fecb432e39bebee
Dave <davebaxtermusic@gmail.com>
2021-10-01 14:04:08 +1300

    includes Navbar and Footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 
+// components
+import Navbar from './nav/Navbar';
+import Footer from './Footer';
+
 // context
 import { usePageMetadata } from '../context/pageMetadata';
 
@@ -10,7 +14,10 @@ export default function Layout({ children, className }) {
   const page = usePageMetadata();
 
   return (
-    <main className={`relative w-screen min-h-screen ${className}`}>
+    <main
+      // className={`relative w-full min-h-screen grid auto-rows-auto ${className}`}
+      className={`relative w-full min-h-screen ${className}`}
+    >
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <meta charSet='utf-8' />
@@ -18,7 +25,9 @@ export default function Layout({ children, className }) {
         <meta name='description' content={page?.description} />
         <link rel='shortcut icon' href='/favicon.ico' />
       </Head>
+      <Navbar />
       {children}
+      <Footer />
     </main>
   );
 }
